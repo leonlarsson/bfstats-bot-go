7c07df3fac7eba33be8b0754642a89646398bfdf
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-08-09 14:45:49 +0000

    extract to bf2042 utils

--- commandhandlers/bf2042/overview.go
+++ commandhandlers/bf2042/overview.go
@@ -2,7 +2,6 @@ package commandhandlers
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/leonlarsson/bfstats-bot-go/canvasdatashapes"
 	create "github.com/leonlarsson/bfstats-bot-go/create/bf2042"
@@ -10,10 +9,10 @@ import (
 	"github.com/leonlarsson/bfstats-bot-go/shared"
 	"github.com/leonlarsson/bfstats-bot-go/utils"
 	"github.com/tdewolff/canvas/renderers"
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
 )
 
+// HandleBF2042OverviewCommand handles the bf2042 overview command.
+// TODO: In the future, this will take a loc param. Similar to bfstats-bot implementation.
 func HandleBF2042OverviewCommand(platform, username string) error {
 	data, err := bf2042datafetcher.FetchBF2042OverviewData(platform, username)
 	if err != nil {
@@ -41,32 +40,32 @@ func HandleBF2042OverviewCommand(platform, username string) error {
 			TimePlayed: canvasdatashapes.Stat{
 				Name:  "Time Played:",
 				Value: overviewSegment.Stats.TimePlayed.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.TimePlayed.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.TimePlayed.Percentile),
 			},
 			Kills: canvasdatashapes.Stat{
 				Name:  "Kills:",
 				Value: overviewSegment.Stats.Kills.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.Kills.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.Kills.Percentile),
 			},
 			Deaths: canvasdatashapes.Stat{
 				Name:  "Deaths:",
 				Value: overviewSegment.Stats.Deaths.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.Deaths.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.Deaths.Percentile),
 			},
 			Assists: canvasdatashapes.Stat{
 				Name:  "Assists:",
 				Value: overviewSegment.Stats.Assists.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.Assists.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.Assists.Percentile),
 			},
 			Revives: canvasdatashapes.Stat{
 				Name:  "Revives:",
 				Value: overviewSegment.Stats.Revives.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.Revives.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.Revives.Percentile),
 			},
 			WlRatio: canvasdatashapes.Stat{
 				Name:  "W/L Ratio:",
 				Value: overviewSegment.Stats.WlPercentage.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.WlPercentage.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.WlPercentage.Percentile),
 			},
 			// BestClass: canvasdatashapes.Stat{
 			// 	Name:  "Best Class:",
@@ -76,25 +75,25 @@ func HandleBF2042OverviewCommand(platform, username string) error {
 			KillsPerMatch: canvasdatashapes.Stat{
 				Name:  "Kills/Match:",
 				Value: overviewSegment.Stats.KillsPerMatch.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.KillsPerMatch.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.KillsPerMatch.Percentile),
 			},
 			KdRatio: canvasdatashapes.Stat{
 				Name:  "K/D Ratio:",
 				Value: overviewSegment.Stats.KdRatio.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.KdRatio.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.KdRatio.Percentile),
 			},
 			KillsPerMinute: canvasdatashapes.Stat{
 				Name:  "Kills/Minute:",
 				Value: overviewSegment.Stats.KillsPerMinute.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.KillsPerMinute.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.KillsPerMinute.Percentile),
 			},
 			MultiKills: canvasdatashapes.Stat{
 				Name:  "Multikills:",
 				Value: overviewSegment.Stats.MultiKills.DisplayValue,
-				Extra: percentileToString(overviewSegment.Stats.MultiKills.Percentile),
+				Extra: utils.PercentileToString(overviewSegment.Stats.MultiKills.Percentile),
 			},
 			Rank: canvasdatashapes.RankStat{
-				Name:    formatRankString(overviewSegment.Stats.Level.Value),
+				Name:    utils.FormatRankString(overviewSegment.Stats.Level.Value),
 				Value:   fmt.Sprintf("%.0f%% to next rank", overviewSegment.Stats.LevelProgression.Value),
 				RankInt: overviewSegment.Stats.Level.Value,
 				Extra:   fmt.Sprintf("XP: %s", overviewSegment.Stats.XPAll.DisplayValue),
@@ -108,40 +107,4 @@ func HandleBF2042OverviewCommand(platform, username string) error {
 	}
 
 	return nil
-}
-
-func percentileToString(percentile *float64) string {
-	if percentile == nil || *percentile == 0.0 {
-		return ""
-	}
-
-	p := message.NewPrinter(language.English)
-	adjustedPercentile := 100 - *percentile
-
-	// If the percentile is a whole number, don't show any decimal places
-	if adjustedPercentile == math.Trunc(adjustedPercentile) {
-		return "Top " + p.Sprintf("%.0f", adjustedPercentile) + "%"
-	}
-
-	// Otherwise, use one decimal place
-	return "Top " + p.Sprintf("%.1f", adjustedPercentile) + "%"
-}
-
-func formatRankString(rank int) string {
-	return fmt.Sprintf("Rank %d (%s)", rank, buildSLevelString(rank))
-}
-
-func buildSLevelString(level int) string {
-	level = level - 99
-	if level < 10 {
-		return fmt.Sprintf("S00%d", level)
-	}
-	if level < 100 {
-		return fmt.Sprintf("S0%d", level)
-	}
-	if level < 1000 {
-		return fmt.Sprintf("S%d", level)
-	}
-
-	return "S001"
 }
--- utils/bf2042.go
+++ utils/bf2042.go
@@ -1,8 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"math"
 	"slices"
 	"strings"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 func IsBaseBF2042Class(game, class string) bool {
@@ -26,4 +31,40 @@ func IsBaseBF2042Class(game, class string) bool {
 	}
 
 	return game == "BF2042" && slices.Contains(bf2042BaseClasses, class)
+}
+
+func buildSLevelString(level int) string {
+	level = level - 99
+	if level < 10 {
+		return fmt.Sprintf("S00%d", level)
+	}
+	if level < 100 {
+		return fmt.Sprintf("S0%d", level)
+	}
+	if level < 1000 {
+		return fmt.Sprintf("S%d", level)
+	}
+
+	return "S001"
+}
+
+func FormatRankString(rank int) string {
+	return fmt.Sprintf("Rank %d (%s)", rank, buildSLevelString(rank))
+}
+
+func PercentileToString(percentile *float64) string {
+	if percentile == nil || *percentile == 0.0 {
+		return ""
+	}
+
+	p := message.NewPrinter(language.English)
+	adjustedPercentile := 100 - *percentile
+
+	// If the percentile is a whole number, don't show any decimal places
+	if adjustedPercentile == math.Trunc(adjustedPercentile) {
+		return "Top " + p.Sprintf("%.0f", adjustedPercentile) + "%"
+	}
+
+	// Otherwise, use one decimal place
+	return "Top " + p.Sprintf("%.1f", adjustedPercentile) + "%"
 }
