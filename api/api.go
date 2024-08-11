242ccb9551d71bac9e782a01bc1d6402f7f09642
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-08-11 16:23:14 +0000

    feat(api): HandleFunc in map

--- api/api.go
+++ api/api.go
@@ -9,9 +9,17 @@ import (
 
 func Start(port string) {
 	r := http.NewServeMux()
-	r.HandleFunc("/bf2042/overview", apihandlers.BF2042OverviewHandler)
-	r.HandleFunc("/bf2042/weapons", apihandlers.BF2042WeaponsHandler)
-	r.HandleFunc("/bf2042/vehicles", apihandlers.BF2042VehiclesHandler)
+
+	handlerMap := map[string]http.HandlerFunc{
+		"/bf2042/overview": apihandlers.BF2042OverviewHandler,
+		"/bf2042/weapons":  apihandlers.BF2042WeaponsHandler,
+		"/bf2042/vehicles": apihandlers.BF2042VehiclesHandler,
+	}
+
+	for route, handlerFunc := range handlerMap {
+		r.HandleFunc(route, handlerFunc)
+	}
+
 	log.Printf("API: Listening on port %s", port)
 	if err := http.ListenAndServe(port, r); err != nil {
 		log.Printf("API: ListenAndServe error: %v", err)
